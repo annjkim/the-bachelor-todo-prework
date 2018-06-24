c0244cfc088065c21769c4922a63cf65c7566087
Ann Kim <annkim822@gmail.com>
2018-06-24 22:43:13 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,7 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |cast|
+    return cast["name"].split(" ")[0] if cast["status"] == "Winner"
+  end
 end
 
 def get_contestant_name(data, occupation)
